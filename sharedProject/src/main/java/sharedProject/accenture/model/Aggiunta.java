8ee2dae51cf0df6509578969a98e85d32a99190e
gianl <gianl@172.16.4.73>
2019-10-09 15:00:56 +0200

    Class Macchina

--- sharedProject/src/main/java/sharedProject/accenture/model/Aggiunta.java
+++ sharedProject/src/main/java/sharedProject/accenture/model/Aggiunta.java
@@ -1,10 +1,16 @@
 package sharedProject.accenture.model;
 
 public class Aggiunta {
-String prova;
-
-
-Sysocasefdsvrsgrsgvòlmfvcasdsa
-System.out.println("dscjdfskjkjs");
+	private String modello;
+	private String cilindrata;
+	
+	public Aggiunta() {
+		this.modello = null;
+		this.cilindrata = null;
+	}
+	public Aggiunta(String modello, String cilindrata) {
+		this.modello = modello;
+		this.cilindrata = cilindrata;
+	}
 
 }
