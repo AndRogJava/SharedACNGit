4787471e0b35a309ca073e5a3cb6a5cdcc6760fa
AndRogJava <rognetta.andrea@yahoo.com>
2019-10-09 14:59:52 +0200

    Commit Aggiunta.java

--- sharedProject/src/main/java/sharedProject/accenture/model/Aggiunta.java
+++ sharedProject/src/main/java/sharedProject/accenture/model/Aggiunta.java
@@ -1,10 +1,41 @@
 package sharedProject.accenture.model;
 
 public class Aggiunta {
-String prova;
+	private String nome;
+	private int cognome;
+	
+	public Aggiunta() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + cognome;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
 
-Sysocasefdsvrsgrsgvòlmfvcasdsa
-System.out.println("dscjdfskjkjs");
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aggiunta other = (Aggiunta) obj;
+		if (cognome != other.cognome)
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+	
 
 }
